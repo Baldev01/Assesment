eb07f8253c3f54acb9856110057aa87b7c8c5924
Baldev01 <baldevkrishanbhardwaj@gmail.com>
2019-05-21 12:09:42 +0530

    myproject

---
+++ 4.js
@@ -0,0 +1,29 @@
+
+{
+  "Resturent": {
+      "name": "Baldev's Hotal",
+    "address": {
+      "BuildingNo": "11" 
+      "street"    : "10 Market Street",
+      "city"      : "San Francisco, CA",
+    },
+    "phone numbers": [
+      "408 338-4238",
+      "408 111-6892"
+    ]
+    "Raring" : "4"
+  }
+{
+  "Resturent": {
+    "name": "Baldev's Hotal",
+    "address": {
+      "BuildingNo": "12" 
+      "street"    : "11 Market Street",
+      "city"      : "San Francisco, CA",
+    },
+    "phone numbers": [
+      "408 338-4238",
+      "408 111-6892"
+    ]
+    "Raring" : "4"
+}
---
+++ 6.js
@@ -0,0 +1,22 @@
+var star="*";
+var i,j;
+
+function looping()
+{
+    
+
+    for(i=0; i<=5; i++ );
+
+    for(j=i+1; j<i; j++);
+
+    console(i);
+
+}
+
+
+
+
+
+/*
+int a = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a-b});*/
